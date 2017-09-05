1343d67f8756a52b7cea4e5ee7eabd2060455907
Brenda Ku <brendasuyin@gmail.com>
2017-09-05 13:37:24 +0800

    <commit>

---
+++ ReverseString.js
@@ -0,0 +1,23 @@
+var myGreeting = "Hello";
+
+	var reverseMyGreeting = myGreeting.split("").reverse().join("");
+
+	
+document.write(myGreeting)
+document.write("<br/>")
+document.write(reverseMyGreeting)
+document.write("<br/>")
+
+
+
+/*for (i=0; i<=myGreeting.length; i++){
+		var result = splitMyGreeting[i];
+	}*/
+
+
+
+var original = '1 3 2';
+var reversed = original.split(' ').reverse().join(' ');
+
+document.write("<br/>")
+document.write(reversed)
